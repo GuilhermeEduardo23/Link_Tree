91b1e5a4e2324ab5e0bc39cf4ebe5b17f725cf95
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-12-12 17:36:20 +0000

    Finish first Deploy v1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,29 @@
 const alterarTema = () => {
     const botao = document.querySelector('.mode');
+    
+    let icon = document.createElement('img');
 
+    const links = document.querySelector('.links');
+    const listLinks = document.querySelector('.list-links');
+
+    //Alteração do conteúdo para dispositivo móvel
     botao.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-        document.querySelector('.principal').classList.toggle('principalDark');
-        document.querySelector('.list-links').classList.toggle('textWhite');
-        document.querySelector('.mode').innerHTML = '<ion-icon name="sunny"></ion-icon>';
-        document.querySelector('.mode').classList.toggle('modeDark');
+        if(document.body.clientWidth <= 767) {
+            document.body.classList.toggle('darkMobile');
+
+            links.classList.toggle('textWhite');
+
+            if(document.body.className === 'darkMobile') {
+                botao.classList.toggle('modeDark');
+                icon.setAttribute('src', '/assets/images/sol.png');
+            } else {
+                icon.setAttribute('src', '/assets/images/lua.png');
+            }
+
+            botao.appendChild(icon);
+        }
     })
+
 }
 
 alterarTema();
