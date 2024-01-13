ce65c2b3d6406feb7e4f58a5dd074b63a3c15d25
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2024-01-13 17:36:15 +0000

    Criação da v3.0 do projeto: adicionado o recurso de troca de tema

--- scripts/alterarTema.js
+++ scripts/alterarTema.js
@@ -1,29 +1,33 @@
-const alterarTema = () => {
-    const botao = document.querySelector('.mode');
+const botao = document.querySelector('.mode');
+const icon = document.createElement('img');
+const links = document.querySelectorAll('.links');
+
+(function createIcon() {
+    icon.setAttribute('src', 'assets/images/lua.png');
+    botao.appendChild(icon);
     
-    let icon = document.createElement('img');
+})();
 
-    const links = document.querySelector('.links');
-    const listLinks = document.querySelector('.list-links');
-
-    //Alteração do conteúdo para dispositivo móvel
+const alterarTema = () => {
     botao.addEventListener('click', () => {
-        if(document.body.clientWidth <= 767) {
+        if(document.body.clientWidth >= 768) {
+            document.body.classList.toggle('darkDesktop');
+        } else {
             document.body.classList.toggle('darkMobile');
-
-            links.classList.toggle('textWhite');
-
-            if(document.body.className === 'darkMobile') {
-                botao.classList.toggle('modeDark');
-                icon.setAttribute('src', '/assets/images/sol.png');
-            } else {
-                icon.setAttribute('src', '/assets/images/lua.png');
-            }
-
-            botao.appendChild(icon);
         }
-    })
 
+        const darkMode = document.body.classList.contains('darkDesktop') || document.body.classList.contains('darkMobile');
+
+        if(darkMode) {
+            icon.setAttribute('src', 'assets/images/sol.png');
+        } else {
+            icon.setAttribute('src', 'assets/images/lua.png');
+        }
+
+        links.forEach((link) => {
+            link.classList.toggle('linksColorWhite');
+        });
+    })
 }
 
 alterarTema();
--- scripts/criarLinks.js
+++ scripts/criarLinks.js
@@ -1,29 +1,28 @@
-function criarLinks() {
+(function criarLinks() {
     const dados = {
         aplicativos: ['E-mail', 'GitHub', 'LinkedIn', 'Meu Currículo', 'WhatsApp'],
         
-        links: ['email.html', 'https://github.com/GuilhermeEduardo23', 'https://www.linkedin.com/in/guilherme-eduardo-rodrigues-da-silva/', 'https://guilhermeeduardo23.github.io/My_Profile/', 'https://api.whatsapp.com/send?phone=351933838876'],
+        link: ['email.html', 'https://github.com/GuilhermeEduardo23', 'https://www.linkedin.com/in/guilherme-eduardo-rodrigues-da-silva/', 'https://guilhermeeduardo23.github.io/My_Profile/', 'https://api.whatsapp.com/send?phone=351933838876'],
 
         icons: ['<ion-icon name="mail-outline"></ion-icon>', '<ion-icon name="logo-github"></ion-icon>', '<ion-icon name="logo-linkedin"></ion-icon>', '<ion-icon name="newspaper-outline"></ion-icon>', '<ion-icon name="logo-whatsapp"></ion-icon>']
     };
 
     //Atribuição via desestruturação (Destructuring assignment)
-    const {aplicativos, links, icons} = dados;
+    const {aplicativos, link, icons} = dados;
 
     const secondSection = document.querySelector('.secondSection');
+    const ul = document.createElement('ul');
 
     for(let i = 0; i < aplicativos.length; i++) {
-        secondSection.innerHTML += `
-            <ul>
-                <li class="list-links">
-                    <a class="links" href="${links[i]}" target="_blank">
-                        ${icons[i]}
-                        ${aplicativos[i]}
-                    </a>
-                </li>
-            </ul>
+        ul.innerHTML += `
+            <li class="list-links">
+                <a class="links" href="${link[i]}" target="_blank">
+                    ${icons[i]}
+                    ${aplicativos[i]}
+                </a>
+            </li>
         `;
     }
-}
 
-criarLinks();
+    secondSection.appendChild(ul);
+})();
